37c11f4f5d4176e77ee0738ddaa4fc2e56ccaf3a
hanafiepratama <hanafiepratama76@gmail.com>
2025-04-19 21:25:16 +0700

    update lagi

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,20 +1,15 @@
-import { Sequelize } from "sequelize";
-import db from "../config/database.js";
-
-const Note = db.define(
-  "notes",{ 
-    title: {type: Sequelize.STRING,},
-    description: {type: Sequelize.STRING,},
-  },
-  {
-    freezeTableName: true,
-    createdAt: "tanggal_dibuat",
-    updatedAt: "tanggal_diperbarui",
-  }
-);
-
+import {Sequelize} from "sequelize";
+import db from "../config/Database.js";
+ 
+const {DataTypes} = Sequelize;
+ 
+const Note = db.define('notes',{
+    number: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT
+});
 export default Note;
 
 (async () => {
-  await db.sync();
-})();
+    await db.sync();
+  })();
