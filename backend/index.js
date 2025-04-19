b68c48af79b6e8a5ce756794c2f9f7e9d2d253ce
hanafiepratama <hanafiepratama76@gmail.com>
2025-04-19 20:27:08 +0700

    tes

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,10 @@
 import express from "express";
 import cors from "cors";
-import db from "./config/Database.js";
-import UserRoute from "./routes/UserRoute.js";
- 
+import notesRouter from "./routes/UserRoute.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(UserRoute);
+app.use(notesRouter);
 
-// Initialize database
-const initializeDB = async () => {
-    try {
-        await db.authenticate();
-        console.log('Database connected...');
-        
-      
-        await db.sync();
-    } catch (error) {
-        console.error('Connection error:', error);
-    }
-};
-
-initializeDB();
- 
-app.listen(5000, ()=> console.log('Server Berjalan...'));
+app.listen(5000, () => console.log("Server Berjalan"));
